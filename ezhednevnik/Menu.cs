3b3ebf1df2deebc9b47af2e7d702b536e6d775ee
hunnbug <feldolshkolnik@gmail.com>
2023-10-20 22:16:13 +0300

    505

---
+++ ezhednevnik/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ezhednevnik
+{
+    internal class Menu
+    {
+        public static int Strelki()
+        {
+            int pos = 3;
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.SetCursorPosition(0, pos);
+                Console.WriteLine("->");
+
+                key = Console.ReadKey();
+                Console.SetCursorPosition(0, pos);
+                Console.WriteLine("  ");
+
+                if (key.Key == ConsoleKey.UpArrow && pos != 3)
+                {
+                    pos--;
+                }
+                if (key.Key == ConsoleKey.DownArrow && pos != 5)
+                {
+                    pos++;
+                }
+            } while (key.Key != ConsoleKey.Enter);
+            return pos;
+        }
+    }
+}
---
+++ ezhednevnik/Program.cs
@@ -0,0 +1,52 @@
+﻿    using ezhednevnik;
+namespace ezhednevnik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime DayNow = DateTime.Now;
+            Console.WriteLine(DayNow);
+            Console.WriteLine("Заметки: ");
+            Console.WriteLine("-----------------------------");
+            int pos = Menu.Strelki();
+            zametki zametka = new zametki();
+            string desc = zametka.Description;
+
+            DateTime date = new DateTime();
+            date = zametki.Date;
+
+            int Showzametki()
+            {
+                ConsoleKeyInfo key;
+                int a = 0;
+
+
+
+                return a;
+            }
+
+            Show();
+            string Show()
+            {
+
+                if (pos == 3)
+                {
+                    desc = "1";
+                }
+                if (pos == 4)
+                {
+                    desc = "2";
+                }
+                if (pos == 5)
+                {
+                    desc = "3";
+                }
+
+                return desc;
+            }
+            Console.WriteLine(desc);
+        }
+        
+    }
+}
